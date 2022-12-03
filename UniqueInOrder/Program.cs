0db26be56e050cec60ceab5d1769e2b726179991
BuildTools <unconfigured@null.spigotmc.org>
2022-12-03 00:51:38 +0000

    Update

---
+++ UniqueInOrder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace UniqueInOrder
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class KataCocinao
+    {
+        public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+        {
+            //your code here...
+
+            return "ASDF";
+        }
+    }
+}
---
+++ __IEnumerateTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace __IEnumerateTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
